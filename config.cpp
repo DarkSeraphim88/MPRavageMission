8d9f14cd93cb594d8ad21694b4efdbef7522aa17
Юрий Рузыбаев < >
2018-03-06 18:25:21 +0500

    changed MenusConfig

--- config.cpp
+++ config.cpp
@@ -20,14 +20,14 @@ class CfgInteractionMenus
             class Lock : MPRavageCusomAction
             {
                 title = "Lock";
-                condition = "(locked DSInteractionObject) isEqualTo 1";
+                condition = "((locked DSInteractionObject) isEqualTo 1) and (DSInteractionObject call HG_fnc_canUnLock)";
                 action = "DSInteractionObject lock true";
             };
 
             class Unlock : MPRavageCusomAction
             {
                 title = "Unlock";
-                condition = "(locked DSInteractionObject) isEqualTo 2";
+                condition = "((locked DSInteractionObject) isEqualTo 2) and (DSInteractionObject call HG_fnc_canUnLock)";
                 action = "DSInteractionObject lock false";
             }
         };
